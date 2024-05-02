7950a4a849e50bc6c787f98649b6b76990bd16a1
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2024-05-01 22:31:02 -0400

    fix action cable test issues and update test

--- test/components/board_list_item_component_test.rb
+++ test/components/board_list_item_component_test.rb
@@ -5,7 +5,8 @@ require "test_helper"
 class BoardListItemComponentTest < ViewComponent::TestCase
   def test_component_renders_something_useful
     board = boards(:one)
-    assert_equal("<div class=\"board__name\"><a href=\"/boards/#{board.id}\">#{board.name}</a></div>", 
-                 render_inline(BoardListItemComponent.new(board: boards(:one))).css(".board__name").to_html)
+    render_inline(BoardListItemComponent.new(board: board))
+    assert_text("Author: #{board.email}", count: 1)
+    assert_text(board.name, count: 1)
   end
 end
